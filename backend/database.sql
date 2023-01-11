93f8910223aea56f552fbf4ac8d96b7f3989cd37
Yhugzee <rodriguez.h@pm.me>
2023-01-11 16:12:10 +0100

    create table car

--- backend/database.sql
+++ backend/database.sql
@@ -11,3 +11,18 @@ CREATE TABLE user
 
 INSERT INTO user (id, firstname, lastname, email, password, phone_number, role)
 VALUES (1, 'Hugo', 'Rodriguez', 'rodriguez.h@pm.me', 'bypass', '0605040302', 'Admin');
+
+CREATE TABLE car
+(
+    id      int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    brand   varchar(255) NOT NULL,
+    model   varchar(255) NOT NULL,
+    mileage INT          NOT NULL,
+    year    INT          NOT NULL,
+    city    varchar(255) NOT NULL,
+    price   INT(10) NOT NULL,
+    bail    INT(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO car (id, brand, model, mileage, year, city, price, bail)
+VALUES (1, 'Renault', 'Clio 5', '4082', '2022', 'Reims', '250', '500');
