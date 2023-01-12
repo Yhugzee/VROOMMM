b923f8989a5393c56de7d7a89730c041427d72b7
Hugo <rodriguez.h@pm.me>
2023-01-13 00:33:03 +0100

    all car component

---
+++ frontend/src/carlist/Utility.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/Car.css";
+
+function Utility() {
+  return (
+    <div>
+      <h2 className="title">CITROËN Ë-JUMPER ÉLECTRIQUE</h2>
+      <img
+        className="img"
+        src="https://www.lys-automobiles.com/img/vehicules/1/239/atb-tmp-36446-1.jpg"
+        alt="Citroën Ë-Jumper"
+      />
+      <div className="features">
+        <h2>Caractéristiques principales</h2>
+        <p>Nombre de place : 9</p>
+        <p>Nombre de cylindre : 4</p>
+        <p>Cylindrée : 1 997 cc</p>
+        <p>Vitesse maximum : 150 km/h</p>
+        <p>Charge maximale : 1890 kg</p>
+      </div>
+    </div>
+  );
+}
+
+export default Utility;
